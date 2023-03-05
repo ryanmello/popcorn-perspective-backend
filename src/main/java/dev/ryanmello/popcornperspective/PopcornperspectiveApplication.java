6d828e3e1422d3517b7471b84607f9084c2b78a8
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-04 18:18:28 -0800

    Initial commit

---
+++ src/main/java/dev/ryanmello/popcornperspective/PopcornperspectiveApplication.java
@@ -0,0 +1,13 @@
+package dev.ryanmello.popcornperspective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornperspectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornperspectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ryanmello/popcornperspective/PopcornperspectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ryanmello.popcornperspective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornperspectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
