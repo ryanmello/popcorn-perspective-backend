97c66611566e73f3a43744bc782dfedcab8f4e33
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-06 20:13:56 -0800

    get list of all movies

---
+++ src/main/java/dev/ryanmello/popcornperspective/MovieRepository.java
@@ -0,0 +1,8 @@
+package dev.ryanmello.popcornperspective;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> { }
